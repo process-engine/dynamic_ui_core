a4c9ba90c600e9b7dd81fe50cbf01eaa15fd8318
MaFa961 <marcel.fasterding@5minds.de>
2019-01-22 12:08:08 +0100

    :sparkles: Add Enum Form Field First Draft

---
+++ src/components/enum-form-field/enum-form-field.tsx
@@ -0,0 +1,30 @@
+/* tslint:disable */
+import { Component, Prop} from '@stencil/core';
+
+@Component({
+  tag: 'enum-form-field',
+  styleUrl: 'enum-form-field.css',
+  shadow: true,
+})
+
+export class DialogFinished {
+
+  @Prop() name: string;
+  @Prop() label: string;
+  @Prop() id: string;
+  @Prop() defaultValue: string;
+
+  @Prop() dynamicUiField: HTMLLabelElement;
+  @Prop() formFields: Array<string>;
+
+  render() {
+    return <div class="form-group">
+              <label for={this.dynamicUiField}>{this.label}:</label>
+              <select class="form-control" id={this.dynamicUiField[this.id]} name={this.formFields[this.id]} placeholder={this.label} value={this.defaultValue}>
+                {/* {{#each enumValues}} */}
+                <option value={this.id}>{this.name}</option>
+                {/* {{/each}} */}
+              </select>
+            </div>;
+  }
+}
