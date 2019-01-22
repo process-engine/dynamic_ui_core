df63dfddb1d79f51029b35cfb93720986628c8ad
MaFa961 <marcel.fasterding@5minds.de>
2019-01-22 12:18:07 +0100

    :sparkles: Add Long form Field First Draft

---
+++ src/components/long-form-field/long-form-field.tsx
@@ -0,0 +1,25 @@
+/* tslint:disable */
+import { Component, Prop} from '@stencil/core';
+
+@Component({
+  tag: 'long-form-field',
+  styleUrl: 'long-form-field.css',
+  shadow: true,
+})
+
+export class DialogError {
+
+  @Prop() label: string;
+  @Prop() id: string;
+  @Prop() defaultValue: string;
+  @Prop() dynamicUiField: HTMLLabelElement;
+  @Prop() formFields: Array<string>;
+
+
+  render() {
+    return <div class="form-group">
+              <label for={this.dynamicUiField[this.id]}>{this.label}:</label>
+              <input type="text" data-inputmask="'mask': '9{+}'" class="form-control" id={this.dynamicUiField[this.id]} name={this.formFields[this.id]} placeholder={this.label} value={this.defaultValue}/>
+            </div>
+  }
+}
