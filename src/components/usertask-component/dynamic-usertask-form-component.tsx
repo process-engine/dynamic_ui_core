9121e569c276a0c51801631243d42d0c993ca944
Robin <robin.lenz@5minds.de>
2019-01-24 18:00:13 +0100

    :sparkles: add submit handler to dynamic form

--- src/components/usertask-component/dynamic-usertask-form-component.tsx
+++ src/components/usertask-component/dynamic-usertask-form-component.tsx
@@ -11,89 +11,17 @@ import {HttpFetchClient} from '../../utils';
   shadow: false,
 })
 export class DynamicUserTaskFormComponent {
-  private userTask: IUserTask;
-
-  @Prop() public correlationId: string;
-  @Prop() public userTaskId: string;
-  @Prop() public accessToken: string;
+  @Prop() public userTask: IUserTask;
 
   @Event() public submited: EventEmitter;
 
-  // tslint:disable-next-line:typedef
-  public async componentWillLoad() {
-    const stringType: any = 'string';
-    const longType: any = 'long';
-    const numberType: any = 'number';
-    const booleanType: any = 'boolean';
-    const dateType: any = 'date';
-    const enumType: any = 'enum';
-
-    const userTaskData: DataModels.UserTasks.UserTaskConfig = {
-      formFields: [{
-        id: 'testText',
-        type: stringType,
-        label: 'Testtext',
-        defaultValue: 'Test1234',
-      },
-      {
-        id: 'testLong',
-        type: longType,
-        label: 'Testlong',
-        defaultValue: '1234',
-      },
-      {
-        id: 'testDecimal',
-        type: numberType,
-        label: 'TestDecimal',
-        defaultValue: '1234,1234',
-      },
-      {
-        id: 'testBoolean',
-        type: booleanType,
-        label: 'TestBoolean',
-        defaultValue: 'true',
-      },
-      {
-        id: 'testDate',
-        type: dateType,
-        label: 'TestDate',
-        defaultValue: '20.12.1983',
-      },
-      {
-        id: 'testEnum',
-        type: enumType,
-        label: 'TestEnum',
-        enumValues: [{id: '1', name: 'Wert1'}, {id: '2', name: 'Wert2'}],
-        defaultValue: '1',
-      }],
-    };
-
-    const userTask: DataModels.UserTasks.UserTask = {
-      id: 'USER-TASK-ID',
-      flowNodeInstanceId: '',
-      name: 'USER-TASK-NAME',
-      correlationId: '',
-      processInstanceId: '',
-      processModelId: '',
-      data: userTaskData,
-      tokenPayload: 'asd',
-    };
-
-    this.userTask = userTask;
-    // const identity: IIdentity = {token: this.accessToken};
-    // const httpClient: IHttpClient = new HttpFetchClient();
-    // const externalAccessor: ExternalAccessor = new ExternalAccessor(httpClient);
-    // const consumerApi: IConsumerApi = new ConsumerApiClientService(externalAccessor);
-    // const userTasksList: UserTaskList = await consumerApi.getUserTasksForCorrelation(identity, this.correlationId);
-    // this.userTask = userTasksList.userTasks.find((u: UserTask) => {
-    //   return u.id === this.userTaskId;
-    // });
-
-  }
-
   private handleSubmit(event: Event): void {
     event.preventDefault();
-    this.submited.emit('bla');
+    this.submited.emit({
+      correlationId: this.userTask.correlationId,
+      processInstanceId: this.userTask.processInstanceId,
+      userTaskId: this.userTask.id,
+    });
   }
 
   // tslint:disable-next-line:typedef
@@ -103,10 +31,6 @@ export class DynamicUserTaskFormComponent {
         <h3 class='card-title'>{this.userTask.name}</h3>
 
         <form onSubmit={(e: Event): void => this.handleSubmit(e)} >
-          <input type='hidden' id='access_token' name='access_token' value={this.accessToken} />
-          <input type='hidden' name='correlationId' value={this.correlationId} />
-          <input type='hidden' name='processInstanceId' value={this.userTask.processInstanceId} />
-          <input type='hidden' name='userTaskId' value={this.userTaskId} />
           <usertask-component userTask={this.userTask}></usertask-component>
           <input type='submit' class='btn btn-primary' value='Abschließen'></input>
         </form>
