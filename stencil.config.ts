7b46f19a5f959c47c93882eef2a55bbc04755715
Robin <robin.lenz@5minds.de>
2019-01-30 15:32:21 +0100

    :bug: fixed transpiling

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import {Config} from '@stencil/core';
 
 export const config: Config = {
   namespace: 'dynamic-usertask-component',
+  srcDir: 'src/components',
   outputTargets: [
     {
       type: 'dist',
