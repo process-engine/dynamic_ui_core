945241fc48be4edf11a1e172c8c03e0021a8d185
Robin <robin.lenz@5minds.de>
2019-02-05 10:26:39 +0100

    :recycle: remove bluebird test

--- src/components/usertask-component/index.ts
+++ src/components/usertask-component/index.ts
@@ -1,13 +1,2 @@
-import * as Bluebird from 'bluebird';
-
-  // By default, Promise-Cancellation is deactivated.
-// We need to activate it here in order to be able to use it.
-Bluebird.config({
-  cancellation: true,
-});
-
-// This will make Bluebird the default Promise implementation throughout the core package.
-global.Promise = Bluebird;
-
 export * from './form-fields/index';
 export * from './dynamic-usertask-component';
