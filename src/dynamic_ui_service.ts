4b2aa26e1ed192b90d489239fa64ecd7e1e308ab
MaFa961 <marcel.fasterding@5minds.de>
2019-02-05 09:00:20 +0100

    :construction: Revert Test

--- src/dynamic_ui_service.ts
+++ src/dynamic_ui_service.ts
@@ -9,15 +9,15 @@ export class DynamicUIService implements IDynamicUIApi {
     return `${__dirname}/assets`;
   }
 
-  // public async getIndex(formKey: string): Promise<any> {
-  //   const template: string = fs.readFileSync(`${__dirname}/templates/index.html`).toString();
+  public async getIndex(formKey: string): Promise<any> {
+    const template: string = fs.readFileSync(`${__dirname}/templates/index.html`).toString();
 
-  //   return Handlebars.compile(template)({form_key: formKey});
-  // }
-  // public async getWebcomponent(formKey: string): Promise<any> {
-  //   const webcomponent: string = fs.readFileSync(`${__dirname}/../dynamic-usertask-component.js`).toString();
+    return Handlebars.compile(template)({form_key: formKey});
+  }
+  public async getWebcomponent(formKey: string): Promise<any> {
+    const webcomponent: string = fs.readFileSync(`${__dirname}/../dynamic-usertask-component.js`).toString();
 
-  //   return webcomponent;
-  // }
+    return webcomponent;
+  }
 
 }
