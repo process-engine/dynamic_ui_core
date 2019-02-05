4a6e0b71bf7b72bc75d5601540de9b7f4fc620d0
MaFa961 <marcel.fasterding@5minds.de>
2019-02-05 08:29:50 +0100

    :construction: Promise Test bpmn-studio

--- src/dynamic_ui_service.ts
+++ src/dynamic_ui_service.ts
@@ -3,7 +3,7 @@ import {IDynamicUIApi} from '@process-engine/dynamic_ui_contracts';
 import * as fs from 'fs';
 import * as Handlebars from 'handlebars';
 
-export class DynamicUIService implements IDynamicUIApi {
+export class DynamicUIService {
 
   public get assetsPath(): string {
     return `${__dirname}/assets`;
