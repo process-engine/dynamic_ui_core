ff7e4f466dbb7c8194155aed343bf0b8ed77b760
MaFa961 <marcel.fasterding@5minds.de>
2019-02-18 09:33:35 +0100

    :construction: Change Regex Test

--- src/components/usertask-component/form-fields/date-form-field.tsx
+++ src/components/usertask-component/form-fields/date-form-field.tsx
@@ -2,7 +2,7 @@ import {DataModels} from '@process-engine/consumer_api_contracts';
 import {Component, State} from '@stencil/core';
 
 import {IFormField} from './iform_field';
-// import {InputValidator} from './input_validator';
+import {InputValidator} from './input_validator';
 
 @Component({
   tag: 'date-form-field',
@@ -19,9 +19,10 @@ export class DateFormField implements IFormField {
     return this.formField.id;
   }
 
-  // private readonly _inputValidator: InputValidator = new InputValidator('(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.(20\d{2}\s*$');
+  private readonly _inputValidator: InputValidator = new InputValidator('^-?\\d*\\,?\\d*$');
+  // new InputValidator('(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.(20\d{2}\s*$');
 
-  public componentWillLoad(): void {
+  public public; public componentWillLoad(): void {
     this.value = this.formField.defaultValue;
   }
 
@@ -29,30 +30,29 @@ export class DateFormField implements IFormField {
     return <div class='form-group'>
       <label htmlFor={this.formField.id}>{this.formField.label}</label>
       <input type='text' data-provide='datepicker' class='form-control'
-        id={this.formField.id} value={this.value} onFocus={(event: any): void => this._handleChange(event)}></input>
+        id={this.formField.id} value={this.value} onFocus={(event: any): void => this._handleChange(event)}
+        onKeyDown={(event: any): void => this._handleKeyDown(event)} onInput={(event: any): void => this._handleInput(event)}></input>
 
     </div>;
   }
 
-  // onKeyDown={(event: any): void => this._handleKeyDown(event)} onInput={(event: any): void => this._handleInput(event)}></input>
+  private _handleInput(event: any): void {
+    const value: string = event.target.value;
 
-  // private _handleInput(event: any): void {
-  //   const value: string = event.target.value;
+    if (this._inputValidator.isValid(value)) {
+      // this.value = parseFloat(value.replace(',', '.'));
+    } else {
+      event.preventDefault();
+    }
+  }
 
-  //   if (this._inputValidator.isValid(value)) {
-  //     // this.value = parseFloat(value.replace(',', '.'));
-  //   } else {
-  //     event.preventDefault();
-  //   }
-  // }
+  private _handleKeyDown(event: any): void {
+    const value: string = this.value + event.key;
 
-  // private _handleKeyDown(event: any): void {
-  //   const value: string = this.value + event.key;
-
-  //   if (this._inputValidator.shouldValidateKey(event.keyCode) && !this._inputValidator.isValid(value)) {
-  //     event.preventDefault();
-  //   }
-  // }
+    if (this._inputValidator.shouldValidateKey(event.keyCode) && !this._inputValidator.isValid(value)) {
+      event.preventDefault();
+    }
+  }
 
   private _handleChange(event: any): void {
     this.value = event.target.value;
