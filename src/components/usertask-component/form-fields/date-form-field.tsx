c11d5ddcce991c835f526ece7393a46f6c891626
MaFa961 <marcel.fasterding@5minds.de>
2019-02-18 12:21:06 +0100

    :construction: Add Test

--- src/components/usertask-component/form-fields/date-form-field.tsx
+++ src/components/usertask-component/form-fields/date-form-field.tsx
@@ -20,9 +20,9 @@ export class DateFormField implements IFormField {
   }
 
   private readonly _reg: any = /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/;
-  private readonly _regValidateSteps: any = /^\s*(((3[01]|[12][0-9]|0?[1-9])\.)|((3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.)|(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2}))\s*$/;
+  private readonly _regCheck: any = /[0-9\.]/;
   private readonly _inputValidator: InputValidator = new InputValidator(this._reg);
-  private readonly _inputValidatorSteps: InputValidator = new InputValidator(this._regValidateSteps);
+  private readonly _inputValidatorCheck: InputValidator = new InputValidator(this._regCheck);
 
   public componentWillLoad(): void {
     this.value = '--.--.----';
@@ -55,12 +55,12 @@ export class DateFormField implements IFormField {
 
   private _handleKeyDown(event: any): void {
     const value: string = this.value + event.key;
-    console.log('## _handleKeyDown ##');
-    console.log('event.key:', event.key);
-    console.log('value:', value);
 
-    if (!this._inputValidatorSteps.isValid(value)) {
-      console.log('Step Valid failed');
+    if (!this._regCheck.isValid(event.key)) {
+      event.preventDefault();
+      console.log('NO Valid Key:', event.key);
+    } else {
+      console.log('Valid Key:', event.key);
     }
 
   }
