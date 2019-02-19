17875d49eba5eae99c81313d4389ab3437920687
MaFa961 <marcel.fasterding@5minds.de>
2019-02-19 08:18:22 +0100

    :construction: Add html Pattern Test

--- src/components/usertask-component/form-fields/date-form-field.tsx
+++ src/components/usertask-component/form-fields/date-form-field.tsx
@@ -19,6 +19,9 @@ export class DateFormField implements IFormField {
     return this.formField.id;
   }
 
+  private readonly _reg: any = /^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$/;
+  private readonly _inputValidator: InputValidator = new InputValidator(this._reg);
+
   public componentWillLoad(): void {
     this.value = this.formField.defaultValue;
   }
@@ -27,13 +30,37 @@ export class DateFormField implements IFormField {
     return (
       <div class= 'form-group'>
         <label htmlFor={this.formField.id}>{this.formField.label}</label>
-        <input type='date' data-provide='datepicker' class='form-control'
-          id={this.formField.id} value={this.value} onFocus={(event: any): void => this._handleChange(event)}></input>
+        <input type='text' pattern='\d{1,2}/\d{1,2}/\d{4}' data-provide='datepicker' class='form-control'
+          id={this.formField.id} value={this.value} onFocus={(event: any): void => this._handleChange(event)}
+          ></input>
+
       </div>
     );
   }
 
+  // onKeyDown={(event: any): void => this._handleKeyDown(event)} onInput={(event: any): void => this._handleInput(event)}
+
   private _handleChange(event: any): void {
     this.value = event.target.value;
+  }
+
+  private _handleInput(event: any): void {
+    const value: string = event.target.value;
+    // if (value.match(this._inputValidator)) {
+    //   console.log('Date is valid');
+    // } else {
+    //   console.log('Date is NOT valid');
+    // }
+    if (this._inputValidator.isValid(value)) {
+      console.log('Date is valid', value);
+    } else {
+      console.log('Date is NOT valid');
+      event.preventDefault();
+    }
+  }
+
+  private _handleKeyDown(event: any): void {
+    const value: string = this.value + event.key;
+
   }
 }
