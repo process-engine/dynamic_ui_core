0b0d1483673748598a236b3a8c3d9fcc08476434
MaFa961 <marcel.fasterding@5minds.de>
2019-02-26 11:25:50 +0100

    :bug: Fix Validation for Date-Form-Field

--- src/components/usertask-component/form-fields/date-form-field.tsx
+++ src/components/usertask-component/form-fields/date-form-field.tsx
@@ -36,19 +36,19 @@ export class DateFormField implements IFormField {
             <label htmlFor={this.formField.id}>{this.formField.label}</label>
             <input type='text' data-provide='datepicker' class='form-control' maxlength='10' placeholder='--.--.----'
             pattern='^(0?[1-9]|[12][0-9]|3[01])([ \.])(0?[1-9]|1[012])\2([0-9][0-9][0-9][0-9])(([ -])([0-1]?[0-9]|2[0-3]):[0-5]?[0-9]:[0-5]?[0-9])?$'
-            id={this.formField.id} value={this.value} onFocus={(event: any): void => this._handleChange(event)}
+            id={this.formField.id} value={this.value} onChange={(event: any): void => this._handleChange(event)}
             onKeyDown={(event: any): void => this._handleKeyDown(event)}></input>
           </div>;
   }
 
   private _handleChange(event: IKeyDownOnInputEvent): void {
     this.value = event.target.value;
+    this.isValid = this._inputValidator.isValidDate(event.target.value);
   }
 
   private _handleKeyDown(event: IKeyDownOnInputEvent): void {
 
     const isValidInput: boolean = this._inputValidator.validateKey(event);
-    this.isValid = isValidInput;
 
     if (isValidInput) {
       return;
